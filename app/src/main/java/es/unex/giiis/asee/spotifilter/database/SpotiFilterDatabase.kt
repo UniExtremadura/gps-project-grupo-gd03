494b714b53a2388ffb4a68547fdd8f644e230e76
Manuel Rodriguez <mrodrigunru@alumnos.unex.es>
2023-12-14 00:44:00 +0100

    GD03JJMS-74 IT03 - IMP - UC02 #done #time 1d 2h 50min

---
+++ app/src/main/java/es/unex/giiis/asee/spotifilter/database/SpotiFilterDatabase.kt
@@ -0,0 +1,34 @@
+package es.unex.giiis.asee.spotifilter.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import es.unex.giiis.asee.spotifilter.data.model.User
+
+@Database(entities = [User::class], version = 1)
+abstract class SpotiFilterDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
+    companion object {
+
+        private var INSTANCE: SpotiFilterDatabase? = null
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+
+        fun getInstance(context: Context): SpotiFilterDatabase? {
+            if (INSTANCE == null) {
+                synchronized(SpotiFilterDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(context, SpotiFilterDatabase::class.java,
+                        "spotifilter.db").build()
+                }
+            }
+            return INSTANCE
+        }
+
+    }
+
+}
