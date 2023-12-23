e6f626e9a62799073d8017fad7f04ef30dcd28a4
Sergio <semolanog@alumnos.unex.es>
2023-12-23 11:05:41 +0100

    GD03JJMS-204 IT05- TEST - UC01 #done #time 2h 42min

---
+++ app/src/androidTest/java/es/unex/giiis/asee/spotifilter/TestUtils.kt
@@ -0,0 +1,135 @@
+package es.unex.giiis.asee.spotifilter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+
+object TestUtils {
+
+    fun performSignUp(username: String, password: String) {
+
+        val materialButton1 = onView(
+            allOf(
+                withId(R.id.logInButton2), withText("Sign up"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    7
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton1.perform(click())
+
+        Thread.sleep(1000)
+
+        val appCompatEditText1 = onView(
+            allOf(
+                withId(R.id.signUpPlainText),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText1.perform(replaceText(username), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.signUpPassword1),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(replaceText(password), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.signUpPassword2),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText(password), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.signUpPassword2), withText(password),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(pressImeActionButton())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.signUpButton), withText("Sign up"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    6
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+
+        Thread.sleep(1000)
+
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+
+        }
+
+    }
+
+}
